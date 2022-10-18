6d98c02995af392d72e65977955befaa59446406
LittleJoes <reallittlejoe@gmail.com>
2022-10-18 18:19:20 +0200

    Hotfix

--- src/main/java/net/suncurve/truevillagers/TrueVillagers.java
+++ src/main/java/net/suncurve/truevillagers/TrueVillagers.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -13,12 +14,13 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.suncurve.truevillagers.item.ModItems;
 import org.slf4j.Logger;
 
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TrueVillagers.MOD_ID)
+        @Mod(TrueVillagers.MOD_ID)
 public class TrueVillagers {
     public static final String MOD_ID = "truevillagers";
     // Directly reference a slf4j logger
@@ -26,8 +28,13 @@ public class TrueVillagers {
 
 
     public TrueVillagers() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+
+        eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
